550ab86023be319a49b4dfdda7902bbb4d6e8e3f
My CMU <mycmu@AnuRajs-Air.lan>
2024-06-13 08:29:49 -0400

    signin/signup/header

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
+# NETFLIX GPT
+
 # Creating React Project
     npx create-react-app nextflix-gpt
 
 # Tailwind CSS Configuration
     - npm install -D tailwindcss
       npx tailwindcss init
+
+# Formik
+    - Form Validations
---
+++ src/components/Login.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import Header from './Header'
+
+const Login = () => {
+
+  const [isSignInForm, setIsSignInForm] = useState(true)
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm)
+  }
+
+  return (
+    <div>
+      <Header />
+        <div className='absolute'>
+          <img src="https://assets.nflxext.com/ffe/siteui/vlv3/51c1d7f7-3179-4a55-93d9-704722898999/03bc4793-4207-4e21-aaec-671f5298b7cd/US-en-20240610-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt='bg-logo'/>
+        </div>
+        <form className='absolute p-12 bg-black w-4/12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
+          <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
+          {
+            !isSignInForm ? <input type='text' placeholder='Full Name' className='p-4 my-4 w-full bg-gray-700'/> : <></>
+          }
+          <input type='text' placeholder='Email Address' className='p-4 my-4 w-full bg-gray-700'/>
+          <input type='password' placeholder='Password' className='p-4 my-4 w-full bg-gray-700'/>
+          <button className='p-4 my-4 bg-red-700 w-full rounded-lg'>{isSignInForm ? "Sign In" : "Sign Up"}</button>
+          <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix? Sign Up Now" : "Already Registerd, Login In Now"}</p>
+        </form>
+    </div>
+  )
+}
+
+export default Login
